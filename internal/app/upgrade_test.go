4c6ddffb634229a870befa15ee3ca18a97b8e753
Khorea1 <141942237+Khorea1@users.noreply.github.com>
2026-09-24 19:10:51 -0300

    test(go): drop ineffective tempdir workaround

--- internal/app/upgrade_test.go
+++ internal/app/upgrade_test.go
@@ -84,13 +84,9 @@ func runUpgradeCommand(t *testing.T, extraEnv []string, flags ...string) (int, s
 // would correctly be treated as an unknown installation identity.
 func writeFakeUpgradeBinaries(t *testing.T, names ...string) []string {
 	t.Helper()
-	root := t.TempDir()
-	binDir := filepath.Join(root, "bin")
-	if err := os.MkdirAll(binDir, 0755); err != nil {
-		t.Fatal(err)
-	}
+	binDir := t.TempDir()
 
-	moduleDir := filepath.Join(root, "module")
+	moduleDir := t.TempDir()
 	cmdDir := filepath.Join(moduleDir, "cmd", "stringer")
 	if err := os.MkdirAll(cmdDir, 0755); err != nil {
 		t.Fatal(err)
@@ -106,10 +102,7 @@ func main() {}
 		t.Fatal(err)
 	}
 
-	buildDir := filepath.Join(root, "build")
-	if err := os.MkdirAll(buildDir, 0755); err != nil {
-		t.Fatal(err)
-	}
+	buildDir := t.TempDir()
 	buildMod := "module depengine.test/upgradefixture\n\ngo 1.27\n\nrequire golang.org/x/tools v0.1.0\n\nreplace golang.org/x/tools => " + filepath.ToSlash(moduleDir) + "\n"
 	if err := os.WriteFile(filepath.Join(buildDir, "go.mod"), []byte(buildMod), 0600); err != nil {
 		t.Fatal(err)
